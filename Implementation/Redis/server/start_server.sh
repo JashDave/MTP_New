31c8accf1a2cae12daa385af413be5dbd8b53757
JashDave <jashdave23@gmail.com>
2016-09-28 01:10:24 +0530

    Updated Redis start_server.sh, added parameter IPAddress

--- Implementation/Redis/server/start_server.sh
+++ Implementation/Redis/server/start_server.sh
@@ -1,10 +1,10 @@
-if [ $# -lt 1 ]
+if [ $# -lt 2 ]
 then
-	echo "Port number required as parameter"
+	echo "IPAddress and Port number required as parameter"
 else
 	sdir=serverinstance
 	mkdir -p $sdir
-	sed -e "s/PORTNUM/$1/g" redis.conf > $sdir/redis.conf
+	sed -e "s/IPADDRESS/$1/g" -e "s/PORTNUM/$2/g" redis.conf > $sdir/redis.conf
 	cd $sdir
 	../redis-3.2.4/src/redis-server ./redis.conf
 fi
